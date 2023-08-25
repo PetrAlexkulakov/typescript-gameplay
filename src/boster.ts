613ee2c14d9bc7d988a0b5b08ed3a7cc963db910
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-08-25 17:54:44 +0400

    fix: fix getBoosterLoot

--- src/boster.ts
+++ src/boster.ts
@@ -11,16 +11,23 @@ export class Booster {
         this.numberOfItems = settings.numberOfItems;
     }
 
+    private getAmountOfRearityItems(): number {
+        return Math.floor(this.numberOfItems * (1 - this.rarity * 0.25))
+    }
+
     getBoosterLoot(playerInventory: IInventory): Item[] {
         const loot: Item[] = [];
         
         for (let i = 0; i < this.numberOfItems; i++) {
-            const rarity = i < this.numberOfItems * 2 / 3 - 1 || this.rarity === 0 ? this.rarity : this.rarity - 1
+            const rarity = i < this.getAmountOfRearityItems() || this.rarity === 0 ? 
+                this.rarity : 
+                this.rarity - 1
             let newItem: Item = itemsBase[0]
             let itemID = 0
 
             do {
-                itemID = Math.floor(Math.random() * Object.values(itemsBase).length)
+                itemID = Math.ceil(Math.random() * Object.values(itemsBase).length)
+                console.log(itemID)
                 newItem = itemsBase[itemID];
             } while (newItem.rarity !== rarity)
 
@@ -31,9 +38,10 @@ export class Booster {
                 playerInventory[itemID] = 1;
             }
         }
-
+ 
         return loot;
     }
 }
 
 console.log(new Booster({ rarity: RARITY.RARE, numberOfItems: 5 }).getBoosterLoot({}))
+console.log(new Booster({ rarity: RARITY.LEGENDARY, numberOfItems: 4 }).getBoosterLoot({}))
